2fe3b44974d18f39389a25b36a96c0d8200f039f
Dom <dominicsmith1021@gmail.com>
2019-04-11 18:59:11 -0400

    Updates

---
+++ precache-manifest.0830704541f5776cef6ebee63ba70b81.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8d83c22dcc2611df7753",
+    "url": "/Memory-Game/static/js/runtime~main.81d8dd5c.js"
+  },
+  {
+    "revision": "a95636b6ceeca5d08428",
+    "url": "/Memory-Game/static/js/main.26c5ec9a.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/Memory-Game/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "a95636b6ceeca5d08428",
+    "url": "/Memory-Game/static/css/main.fe738a2b.chunk.css"
+  },
+  {
+    "revision": "6e5f3579497810e43d96c7b7f9fc350e",
+    "url": "/Memory-Game/index.html"
+  }
+];
