3eaa767ea97941740898b1f49920d359d946ecea
Dom <dominicsmith1021@gmail.com>
2019-04-11 18:18:15 -0400

    Added navbar & background image

--- src/App.js
+++ src/App.js
@@ -1,27 +1,30 @@
 import React, { Component } from "react";
 import CharacterCard from "./components/CharacterCard";
+import NavBar from "./components/NavBar";
 import Wrapper from "./components/Wrapper";
 import characters from "./characters.json";
 import "./App.css";
+
+const defaultMsg = "Click on an image to begin!";
 
 class App extends Component {
   state = {
     characters,
     alreadyChosen: [],
-    result: "correct",
+    result: "",
     score: 0
   };
 
   checkAnswer = id => {
     if (this.state.alreadyChosen.indexOf(id) !== -1) {
       this.setState({ 
-        result: "incorrect",
+        result: "You answered incorrectly!",
         score: 0 
       });
     } else {
       this.setState({
         alreadyChosen: this.state.alreadyChosen.concat(id),
-        result: "correct",
+        result: "You answered correctly!",
         score: this.state.score + 1
       });
     }
@@ -31,8 +34,10 @@ class App extends Component {
   render() {
     return (
       <Wrapper>
-        <h1 className="title">Your answer is {this.state.result}</h1>
         <div className="container">
+          <div className="row navbar-background">
+            <NavBar result={this.state.score === 0 ? defaultMsg : this.state.result} score={this.state.score} />
+          </div>
           <div className="row">
             {this.state.characters.sort(() => (.5 - Math.random())).map(character => (
               <CharacterCard
